d4d2ff132d21d6900590667832a1b57416491e02
istiaqfuad <istiaqurrahmanfuad@gmail.com>
2025-04-17 04:14:24 +0600

    store the todos in a json file for persistence

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 func main() {
 	todos := Todos{}
-	todos.Add("Buy Milk")
-	todos.Add("Buy Bread")
-
-	todos.Toggle(0)
-
-	todos.Print()
+    storage := NewStorage[Todos]("todos.json")
+    storage.Load(&todos)
+    todos.Add("Buy Milk")
+    todos.Add("Buy Bread")
+    todos.Toggle(0)
+    todos.Print()
+    storage.Save(todos)
 }
